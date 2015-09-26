e25d924f1f90c83bdabc85187c041686e9c7268b
Kevin Kientz <KJKientz@users.noreply.github.com>
2015-09-26 11:52:14 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,21 @@
 
-# makeCacheMatrix returns a list of functions - set, get, setInverse abd getInverse. 
-# cacheSolve returns the inverse of a square matrix
-# The two functions can be used together by first using makeCacheMatrix() as an anonymous function stored in a variable, and then using the set() function to set a square matrix that you want to invert.
+## OVERALL DESCRIPTION
+# makeCacheMatrix returns a list of functions - set(), get(), setInverse() abd getInverse(). 
+# cacheSolve returns the inverse of a square matrix - either a cached version of the inverse, 
+# or it computes the inverse if a cached version does not exist.
+# The two functions can be used together by first using makeCacheMatrix() as an anonymous function stored in a variable
+# and then using the set() function as an argument of the variable you have created. This sets a square matrix that you want to invert.
 # For e.g., you can set c <- makeCacheMatrix(); c$set(matrix(c(7,8,10,12),nrow=2, ncol=2));
 # Then, when you run cacheSolve(c), you will get the inverse of the matrix created using the set command.
-# In the event that the matrix had already been inverted using the setInverse function contained in makeCacheMatrix, R will get the cached inverse instead of re-calculating, and it will print out "getting cached inverse of matrix"
+# In the event that the matrix had already been inverted using the setInverse function contained in makeCacheMatrix, 
+# R will get the cached inverse instead of re-calculating, and it will print out "getting cached inverse of matrix"
 # The whole function could be written in one line, like this:
 # cacheSolve(makeCacheMatrix(matrix(c(7,8,10,12), nrow=2, ncol=2)))
-# It should be noted that if you run the function all in one line, then it doesn't actually get the cached version if one exists. I'm not sure why but I'm assuming it has something to do with R's lexical scoping.
+# However, because the one-line function is run all at the same time, it will not actually find a cached version of the matrix.
+# This is due to R's lexical scoping. So if you want R to use a cached version of the matrix, you must run the functions separately.
 
 
-
+## MakeCacheMatrix() description
 # MakeCacheMatrix is to be used as anonymous function that will be stored in a variable. 
 # This variable, if set by using the set function contained within this makeCacheMatrix function, will be passed through the cacheSolve function.
 # When it is passed through the cacheSolve function, that function will use a cached version of inverted matrix (through the getInverse variable)
@@ -38,8 +43,11 @@ list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
+## CacheSolve() description
 # cacheSolve is a function that takes as an argument a square matrix and returns the inverse. 
-# The inverse will either be calculated by R or it will use a cached version of the inverse if it is already stored in R's memory.
+# The inverse will either be calculated by R or it will use a cached version of the inverse 
+# if it is already stored in R's memory. If the square matrix is not invertible, the function
+# instead returns a message that says that it is not invertible.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
@@ -52,11 +60,16 @@ cacheSolve <- function(x, ...) {
         }
         
         # If cached inverse doesn't exist, R will find the inverse of the square matrix using the 'solve' function.
+        # If square matrix is not invertible, R will print a message indicating that the matrix is not invertible.
         matrix1 <- x$get()
+        if(!det(matrix1) == 0) {
         m <- solve(matrix1, ...)
         x$setInverse(m)
         
-        m
+       m        
+       } else {
+                message("Matrix is not invertible. Determinant of matrix is 0.")
                 
-        
+                }
+
 }
